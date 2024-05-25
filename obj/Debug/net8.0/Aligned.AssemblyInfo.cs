890637bb2761fcb3180ec24116f5863550345ff6
Habibserhan <91726569+Habibserhan@users.noreply.github.com>
2024-05-25 21:57:22 +0300

    db

--- obj/Debug/net8.0/Aligned.AssemblyInfo.cs
+++ obj/Debug/net8.0/Aligned.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aligned")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b28d877e25096da727854293dbb5f329daaf57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d2934a0d4bbe16e72e654ebc8481e447d61b19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aligned")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aligned")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
